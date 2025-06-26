3af7f83246de6cfa66c1fee924a858e65d9bd42b
MikolajGrzelak <mikolaj.grzelak@paycomplete.com>
2025-06-26 12:21:31 +0200

    Dodano andon-signals.suzo.int do CSRF_TRUSTED_ORIGINS

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,6 +132,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "https://andon-signals.suzo.int",
+    "http://andon-signals.suzo.int",   # dodaj też http, jeśli reverse proxy nie używa https do backendu
 ]
 
 
